d72a2836786d9cea65d9103cbdb1341a3d545d13
Kanaya Ozora <163905596+nayachewsudon@users.noreply.github.com>
2026-02-17 15:02:22 +0100

    Refactor reset signal and memory interface logic
    Updated reset signal from 'rst' to 'rst_n' and modified memory address assignments to include depth considerations. Adjusted data memory write enable to be 4 bits. Note: Address range is now 16 KB

--- Pipelined Processor/rv_pl_wrapper.v
+++ Pipelined Processor/rv_pl_wrapper.v
@@ -1,16 +1,14 @@
-`include "top_module.v"
-
 module rv_pl_wrapper (
     input clk,
-    input rst,
+    input rst_n,
 
     //IMEM Interface
-    output [31:0] imem_addr, //Goes from wrapper to IBRAM
+    output [31:0] imem_addr, //Goes from wrapper to IBRAM 'only 11 bits are used 2048 words bram '
     input [31:0] imem_dout,  //IBRAM to wrapper (to be sent to top module)
-
+ 
     //DMEM Interface
     output [31:0] dmem_addr, 
-    output dmem_we, 
+    output [3:0] dmem_we, // vivado bram has it as 4bits
     output [31:0] dmem_din, 
     input [31:0] dmem_dout,
     output reg done_flag
@@ -24,7 +22,7 @@ wire [31:0] M_WriteData;
 
 rv_pl rv_pl (
     .clk(clk),
-    .rst_n(rst),
+    .rst_n(rst_n),
     .F_pc(F_pc),
     .F_instr(imem_dout),
     .M_ALUResult(M_ALUResult),
@@ -34,17 +32,17 @@ rv_pl rv_pl (
 );
 
 //Connecting the imem interface
-assign imem_addr = F_pc;
+assign imem_addr = {18'b0, F_pc[13:2]}; // not sure if we should consider the depth or this is okay
 
 //Connecting the data memory interface
-assign dmem_addr = M_ALUResult;
-assign dmem_we = M_MemWrite;
+assign dmem_addr = {18'b0, M_ALUResult[13:2]};
+assign dmem_we = M_MemWrite ? 4'b1111 : 4'b0000 ; //dmem_we is 4 bits, M_MemWrite is 1 bit, this is how we'll translate it
 assign dmem_din = M_WriteData;
 
 //Signal done flag when program writes DEADBEEF into 0x2000
-always @(posedge clk or negedge rst) begin
-    if (!rst) done_flag <= 1'b0;
-    else if (dmem_we && dmem_addr == 32'h00002000 && dmem_din == 32'hDEADBEEF) 
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) done_flag <= 1'b0;
+    else if (M_MemWrite && M_ALUResult == 32'h00002000 && M_WriteData == 32'hDEADBEEF) 
         done_flag <= 1'b1;
 end
 
