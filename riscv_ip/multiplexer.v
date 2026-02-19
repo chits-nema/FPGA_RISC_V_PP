f7421fefa59adca468fea2d4978e5d84ece782e3
chits-nema <cvnemazuwa@gmail.com>
2026-02-19 15:20:36 +0100

    unstaged changes

---
+++ riscv_ip/multiplexer.v
@@ -0,0 +1,26 @@
+module mux2 #(parameter WIDTH = 8)(
+    input [WIDTH-1:0] d0, d1,
+    input s,
+    output [WIDTH-1:0]y
+);
+    assign y = s ? d1: d0;    
+
+endmodule
+
+module mux3 #(parameter WIDTH = 8)(
+    input [WIDTH-1:0] d0, d1, d2,
+    input [1:0] s,
+    output [WIDTH-1:0] y
+);
+
+    assign y = s[1] ? d2 : (s[0]? d1:d0);
+
+endmodule
+
+module mux4 #(parameter WIDTH = 8)(
+    input [WIDTH-1:0] d0, d1,d2,d3,
+    input [1:0] s,
+    output [WIDTH-1:0] y
+);
+    assign y =s[1]?(s[0]?d3:d2):(s[0]?d1:d0);
+endmodule
