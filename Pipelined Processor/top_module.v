d49ddf411cff26e521f3cf0b038d858cfa3e1571
Kanaya Ozora <163905596+nayachewsudon@users.noreply.github.com>
2026-02-20 11:15:44 +0000

    Upload test python notebook

--- Pipelined Processor/top_module.v
+++ Pipelined Processor/top_module.v
@@ -272,7 +272,7 @@ adder pc_target_add(
 execute_memory_reg PLR3(
     .clk(clk),
     .rst_n(rst_n),
-    .flush(E_PCSrc),  // Flush EX->MEM only on taken branch/jump (use E_PCSrc)
+    .flush(1'b0),  // Not used for control hazards (branches/jumps) - only for exceptions
     // Control signals
     .RegWriteE(E_RegWrite),
     .ResultSrcE(E_ResultSrc),
